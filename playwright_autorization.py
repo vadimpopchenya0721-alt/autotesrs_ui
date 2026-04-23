3861d84897c05c523a9d45940cce904dd9a1dde8
Vadim Popchenya <vadim.popchenya0721@gmail.com>
2026-04-23 15:38:31 +0300

    Add tests

--- playwright_autorization.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import sync_playwright, expect
-
-with sync_playwright() as playwright:
-    chromium = playwright.chromium.launch()
-
---
+++ test/test_playwright_autorization.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_wrong_email_or_password_authorization():
+    with sync_playwright() as playwright:
+        chromium = playwright.chromium.launch(headless=False)
+        page = chromium.new_page()
+        page.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+        email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
+        email_input.fill("test@mail.ru")
+
+        password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
+        password_input.fill("test")
+
+        login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+        login_button.click()
+
+        wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+        expect(wrong_email_or_password_alert).to_be_visible()
+
+        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password ")
