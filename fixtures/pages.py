cb95e3a2260305cdab91bb643b7e9c1a9e493927
Vadim Popchenya <vadim.popchenya0721@gmail.com>
2026-05-01 12:57:07 +0300

    Практикуемся в реализации PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,8 +1,32 @@
 import pytest
 
 from playwright.sync_api import Page
+
+from pages.course_list_page import CourseListPage
+from pages.create_course_page import CreateCoursePage
+from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
+
 
 @pytest.fixture
-def login_page(chromium_page:Page) -> LoginPage:
+def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture
+def registration_page(chromium_page: Page) -> RegistrationPage:
+    return RegistrationPage(page=chromium_page)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
+
+@pytest.fixture
+def course_list_page(chromium_page_with_state:Page) -> CourseListPage:
+    return CourseListPage(page=chromium_page_with_state)
+@pytest.fixture
+
+def create_course_page(chromium_page_with_state:Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
---
+++ test/test_successful_registration.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Page
+
+import pytest
+
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+
+@pytest.mark.registration
+@pytest.mark.regression
+def test_successful_registration(registration_page:RegistrationPage,dashboard_page:DashboardPage):
+
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(
+        email="user.name@gmail.com",
+        username="username",
+        password="password"
+    )
+    registration_page.click_registration_button()
+    dashboard_page.check_visible_dashboard_title()
+
+
